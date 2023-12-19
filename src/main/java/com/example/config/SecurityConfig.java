534a1986d82ae5a61c9a95fc5c04b8f933002ae9
rk0720 <ryotakatsumataax@gmail.com>
2023-12-19 22:31:42 +0900

    lesson3
    課題5

---
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception {
+    // 認証・認可
+		http.authorizeRequests()
+			.antMatchers("/loginForm").permitAll() //loginformはだれでもアクセス可
+	    	.anyRequest().authenticated(); //それ以外はログインを求めるというメソッド
+	//ログイン
+		http.formLogin()
+			.loginProcessingUrl("/login")
+			.loginPage("/loginForm")
+			.usernameParameter("email")
+			.passwordParameter("password")
+			.defaultSuccessUrl("/library", true)
+			.failureUrl("/loginForm?error");
+		
+		//ログアウト
+		http.logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/loginForm");
+	}
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    // WebSecurity型の引数を持ったconfigure()を追加します
+    public void configure(WebSecurity web) throws Exception {
+        /** 以下のファイルパス配下のディレクトリ、ファイルすべてを認証・認可の対象から除外します
+            src/main/resources/static/css/
+            src/main/resources/static/js/
+            src/main/resources/static/images/
+        */
+        web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+    }
+}
--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -3,11 +3,10 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entity.Library;
 import com.example.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<Library, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
     // 以降の設問で必要に応じて機能を実装すること
 	public User findByEmail(String email);
 }
